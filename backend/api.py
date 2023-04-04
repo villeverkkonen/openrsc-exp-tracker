bbc17411d69244d3234c7f0c5996a9b84e9dea87
Ville Verkkonen <villeverkkonen@gmail.com>
2023-04-05 01:18:10 +0300

    Test data and UI improvements

--- backend/api.py
+++ backend/api.py
@@ -2,6 +2,8 @@ from flask import Flask
 import requests
 from bs4 import BeautifulSoup
 from players import players
+from hiscore import Hiscore
+import random
 
 app = Flask(__name__, static_folder='../client/dist', static_url_path='/')
 
@@ -13,10 +15,15 @@ def root():
 def get_hiscores():
     hiscores = []
     for player in players:
-        new_exp = get_new_exp_from_hiscores(player['playerName'])
-        parsed_new_exp = parse_exp(new_exp) 
+        # new_exp = get_new_exp_from_hiscores(player['playerName'])
+        # parsed_new_exp = parse_exp(new_exp)
+        old_exp = round(random.uniform(0, 100), 2)
+        new_exp = round(random.uniform(100, 200), 2)
+        # hiscores.append(
+        #     { 'playerName': player['playerName'], 'oldExp': player['oldExp'], 'newExp': parsed_new_exp, 'gainedExp': parsed_new_exp - player['oldExp'] })
         hiscores.append(
-            { 'playerName': player['playerName'], 'oldExp': player['oldExp'], 'newExp': parsed_new_exp })
+            { 'playerName': player['playerName'], 'oldExp': old_exp, 'newExp': new_exp, 'gainedExp': new_exp - player['oldExp'] })
+    hiscores.sort(key=lambda x: x['gainedExp'], reverse=True)
     return hiscores
 
 def get_new_exp_from_hiscores(player_name):
--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,37 +1,62 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
+  import { onMount } from "svelte";
 
-  let hiscores = []
+  let hiscores = [];
 
-  onMount(async() => {
+  onMount(async () => {
     await getHiscores();
-  })
+  });
 
   async function getHiscores() {
-    let response = await fetch('/api/hiscores', {
-      method: 'GET',
+    let response = await fetch("/api/hiscores", {
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-    })
+    });
 
-    let result = await response.json()
-    console.log(result)
-    hiscores = result
+    let result = await response.json();
+    console.log(result);
+    hiscores = result;
   }
 </script>
 
 <main>
   <h1>OpenRSC Skilling Competition</h1>
-  {#each hiscores as { playerName, oldExp, newExp }}
-    <div>
-      <p>Player: {playerName}</p>
-      <p>Old exp: {oldExp}</p>
-      <p>New exp: {newExp}</p>
+  {#each hiscores as { playerName, oldExp, newExp, gainedExp }}
+    <div class="playerCard">
+      <p class="playerName">{playerName}</p>
+      <div class="expContainer">
+        <span class="exp">Old exp: {oldExp}</span>
+        <span class="exp expGained">Gained: {gainedExp}</span>
+        <span class="exp">New exp: {newExp}</span>
+      </div>
     </div>
   {/each}
 </main>
 
 <style>
+  .playerCard {
+    background-color: black;
+    padding: 3px;
+    margin: 5px;
+    border-radius: 5px;
+  }
+  .playerName {
+    text-align: center;
+  }
 
+  .expContainer {
+    display: flex;
+    text-align: center;
+  }
+
+  .exp {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+
+  .expGained {
+    color: red;
+  }
 </style>
