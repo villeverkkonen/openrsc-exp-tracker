171f6db96dc5405edf9e06dac35a6e42e5b51ef1
Ville Verkkonen <villeverkkonen@gmail.com>
2023-04-05 20:02:11 +0300

    Updated players starting experiences

--- backend/players.py
+++ backend/players.py
@@ -1,5 +1,5 @@
 players = [
-    { 'playerName': 'Lord Jolt', 'oldExp': 0 },
-    { 'playerName': 'Purilainen', 'oldExp': 0 },
-    { 'playerName': 'LeChuck', 'oldExp': 0 },
+    { 'playerName': 'Lord Jolt', 'oldExp': 2337.597 },
+    { 'playerName': 'Purilainen', 'oldExp': 433.843 },
+    { 'playerName': 'LeChuck', 'oldExp': 158.362 },
 ]
