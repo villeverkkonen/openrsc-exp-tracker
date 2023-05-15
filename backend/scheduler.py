726549bc70358bf178220dc2bb2cc6e0344a7d82
Ville Verkkonen <villeverkkonen@gmail.com>
2023-05-15 12:27:45 +0300

    Refactor scheduler http requests

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -21,37 +21,36 @@ async def update_hiscores():
     players = []
     async with httpx.AsyncClient() as client:
         GET_PLAYERS_URL = API_BASE_URL + "/api/players"
-        players_response = await client.get(GET_PLAYERS_URL)
+        players_response = await client.get(GET_PLAYERS_URL, timeout=10.0)
         players = players_response.json()
 
-    for index, player in enumerate(players):
-        print('Updating hiscores for player: ' + player['name'])
-        new_exp = get_new_exp_from_hiscores(player['name'])
-        parsed_new_exp = parse_exp(new_exp)
-        total_gained_exp = parsed_new_exp - player['original_exp']
+        for index, player in enumerate(players):
+            print('Updating hiscores for player: ' + player['name'])
+            new_exp = await get_new_exp_from_hiscores(player['name'], client)
+            parsed_new_exp = parse_exp(new_exp)
+            total_gained_exp = parsed_new_exp - player['original_exp']
 
-        hiscore = {"new_exp": parsed_new_exp,
-                   "total_gained_exp": total_gained_exp}
-        async with httpx.AsyncClient() as client:
+            hiscore = {"new_exp": parsed_new_exp,
+                       "total_gained_exp": total_gained_exp}
             CREATE_HISCORE_URL = API_BASE_URL + \
                 "/api/players/" + str(player['id']) + "/hiscores"
-            hiscore_response = await client.post(CREATE_HISCORE_URL, json=hiscore)
+            hiscore_response = await client.post(CREATE_HISCORE_URL, json=hiscore, timeout=10.0)
             print("Created hiscore:")
             print(hiscore_response.json())
 
-        if index < len(players) - 1:
-            # We don't want to burden rsc.vet too much too fast
-            time.sleep(15)
-    print('Hiscores updated!')
+            if index < len(players) - 1:
+                # We don't want to burden rsc.vet too much too fast
+                time.sleep(15)
+        print('Hiscores updated!')
 
 
-def get_new_exp_from_hiscores(player_name):
+async def get_new_exp_from_hiscores(player_name, client):
     trimmed_player_name = player_name
     if ' ' in trimmed_player_name:
         trimmed_player_name = trimmed_player_name.replace(' ', '%20')
 
     URL = "https://rsc.vet/player/preservation/" + trimmed_player_name
-    response = httpx.get(URL)
+    response = await client.get(URL, timeout=10.0)
     soup = BeautifulSoup(response.content, 'lxml')
 
     skill_string = soup.find(lambda tag: tag.name ==
